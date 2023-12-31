91faea65331ef15a0f0f8b0ab010ed91338acd20
MaikonWeber <maikonweber@gmail.com>
2023-12-31 19:10:59 -0300

    Add telegram Send Mensage

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,12 +1,38 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
+
 @Injectable()
 export class TelegramService {
-    private readonly logge = new Logger(TelegramService.name)
-    private readonly key: string;
+    private readonly logger = new Logger(TelegramService.name)
+    private readonly botToken: string;
+    private readonly chatId: string;
+
     constructor(private readonly configService: ConfigService) {
-        this.key = configService.get<string>('TELEGRAM_TOKEN');
+        this.botToken = configService.get<string>('TELEGRAM_TOKEN');
+        this.chatId = this.configService.get<string>('TELEGRAM_CHAT_ID');
+
     }
 
+    async sendMessage(message: string): Promise<void> {
+        const apiUrl = `https://api.telegram.org/bot${this.botToken}/sendMessage`;
+
+        const params = new URLSearchParams({
+            chat_id: this.chatId,
+            text: message,
+        });
+
+        const url = `${apiUrl}?${params}`;
+
+        try {
+            const response = await fetch(url, { method: 'POST' });
+
+            if (!response.ok) {
+                throw new Error(`Failed to send message: ${response.statusText}`);
+            }
+        } catch (error) {
+            // Handle errors appropriately
+            console.error(error.message);
+        }
+    }
 }
